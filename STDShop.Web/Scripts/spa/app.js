660768a3397aeb3e56647cd0c078fe2b42f9a43c
Haulv <luonghau91@gmail.com>
2016-09-05 23:24:47 +0700

    commit service in angular js 05092016

--- STDShop.Web/Scripts/spa/app.js
+++ STDShop.Web/Scripts/spa/app.js
@@ -1,22 +1,24 @@
 ﻿/// <reference path="../plugins/angular/angular.js" />
 var myApp = angular.module('myModule', []);
-//myApp.controller("myController", myController);
-//myController.$inject = ['$scope'];
-//declare
-//function myController($scope) {
-//    $scope.message = "This is my message from controller";
-//}
-myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
 
-function schoolController($scope) {
-    $scope.message = "This is my message from school controller";
+myApp.controller("schoolController", schoolController);
+myApp.service('Validator', Validator);
+schoolController.$inject = ['$scope', 'Validator'];
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber(10);
+    }
+    $scope.num = 1;
 }
-function studentController($rootScope, $scope) {
-     $scope.message = "This is my message from student controller";
-     //$rootScope.message = "This is my message from student controller";
-}
-function teacherController($scope) {
-    $scope.message = "This is my message from teacher controller";
+function Validator($window) {
+    return {
+        checkNumber:checkNumber
+    }
+    function checkNumber(input) {
+        if (input %2 == 0) {
+           return 'this is even';
+        } else {
+            return 'this is odd';
+        }
+    }
 }
