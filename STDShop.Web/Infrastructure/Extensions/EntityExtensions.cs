919fb6255a523cd394ba8903d147e8a333a030c1
Haulv <luonghau91@gmail.com>
2016-09-13 22:29:36 +0700

    commit management tag product 13/09/2016

--- STDShop.Web/Infrastructure/Extensions/EntityExtensions.cs
+++ STDShop.Web/Infrastructure/Extensions/EntityExtensions.cs
@@ -88,6 +88,7 @@ namespace STDShop.Web.Infrastructure.Extensions
             product.MetaKeyword = productVm.MetaKeyword;
             product.MetaDescription = productVm.MetaDescription;
             product.Status = productVm.Status;
+            product.Tags = productVm.Tags;
         }
     }
 }
