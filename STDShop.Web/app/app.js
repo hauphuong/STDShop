5560af5e2498a688f50686b8206582b358fe6f2c
Haulv <luonghau91@gmail.com>
2016-09-14 21:54:15 +0700

    commit create login form 14/09/2016

--- STDShop.Web/app/app.js
+++ STDShop.Web/app/app.js
@@ -1,17 +1,31 @@
 ﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+
 (function () {
     angular.module('stdshop',
         ['stdshop.products',
-        'stdshop.product_categories',
-        'stdshop.common'])
+         'stdshop.product_categories',
+         'stdshop.common'])
         .config(config);
+
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('home', {
-            url: "/admin",
-            templateUrl: "/app/components/home/homeView.html",
-            controller: "homeController"
-        });
-        $urlRouterProvider.otherwise('/admin')
+        $stateProvider
+            .state('base', {
+                url: '',
+                templateUrl: '/app/shared/views/baseView.html',
+                abstract: true
+            }).state('login', {
+                url: "/login",
+                templateUrl: "/app/components/login/loginView.html",
+                controller: "loginController"
+            })
+            .state('home', {
+                url: "/admin",
+                parent: 'base',
+                templateUrl: "/app/components/home/homeView.html",
+                controller: "homeController"
+            });
+        $urlRouterProvider.otherwise('/login');
     }
 })();
